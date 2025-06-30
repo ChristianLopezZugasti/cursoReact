f7e4417836f7632c349bb3d7a9cb6716f5adb254
ChristianLopezZugasti <chris5051.cl@gmail.com>
2025-06-30 10:35:13 -0600

    cambios varios

--- 02-intro-javascript/src/main.jsx
+++ 02-intro-javascript/src/main.jsx
@@ -3,6 +3,7 @@ const apiKey = 'gzTJvfnEbH7y4mpsJZW4mOSuKeqtbRHS';
 const peticion  = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`)
 
 peticion.then(resp => {
+    resp.json()
     console.log(resp)
 })
 .catch(console.warn)
